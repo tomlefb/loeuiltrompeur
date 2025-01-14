2c53e8186fb3148f1b8b946d46ab1ff794151f17
Tom <tom.lefevrebonzon@gmail.com>
2025-01-14 15:24:55 +0100

    wip

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  enterSite() {
+    this.router.navigate(['/menu']); // Redirige vers la page menu, change si nécessaire
+  }
 }
